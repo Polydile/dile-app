f8d24e84e0b4a884e90dff2de3961c977f625eff
midesweb <malvarez@desarrolloweb.com>
2022-07-21 01:05:52 +0200

    change email confirmation message

--- src/components/user/dile-resend-confirmation-email.js
+++ src/components/user/dile-resend-confirmation-email.js
@@ -8,9 +8,9 @@ export class DileResendConfirmationEmail extends AuthMixin(LitElement) {
     css`
       :host {
         display: block;
-        --dile-message-background-color: var(--primary-darker-color);
-        --dile-button-background-color: var(--secondary-color);
-        --dile-button-text-color: var(--secondary-text-color);
+        --dile-message-background-color: var(--secondary-color);
+        --dile-button-font-size: 0.8rem;
+        --dile-message-padding: 1rem 2rem;
       }
       a {
         color: #ffc;
@@ -22,11 +22,13 @@ export class DileResendConfirmationEmail extends AuthMixin(LitElement) {
     return html`
     ${this.user && !this.user.email_verified_at
       ? html`
-        <dile-message hideCloseIcon opened fixedOnBottom>
+        <dile-message hideCloseIcon opened position="right-bottom">
+          <h2>You are logged-in</h2>
           <p>
-            Your user is not verified. Please check your email and click on the confirmation link.
-            <br><br>
-            <dile-button @click="${this.resendConfirmation}">Resend confirmation email</dile-button>
+            Before start using the app you need to verify your email account. We have sent an email to you. <b>Please check your email and click on the confirmation link</b>.
+          </p>
+          <p>
+            If you couldn't find the email click on <a href="#" @click="${this.resendConfirmation}">resend the confirmation email</a>.
           </p>
           
         </dile-message>
